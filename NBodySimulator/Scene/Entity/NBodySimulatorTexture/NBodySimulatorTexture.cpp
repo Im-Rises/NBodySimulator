0e725751bea64aff1b31b19cb4c53e4b27feb3b0
Im-Rises <quentin-morel88@hotmail.com>
2023-06-24 01:39:09 -0400

    wip

--- NBodySimulator/Scene/Entity/NBodySimulatorTexture/NBodySimulatorTexture.cpp
+++ NBodySimulator/Scene/Entity/NBodySimulatorTexture/NBodySimulatorTexture.cpp
@@ -18,7 +18,7 @@ const char* const NBodySimulatorTexture::VertexShaderPhysicSource =
 
     void main() {
         gl_Position = vec4(a_position, 1.0);
-        v_texCoord = a_position.xy;
+        v_texCoord = a_position.xy * 0.5 + 0.5;
     }
 )";
 
